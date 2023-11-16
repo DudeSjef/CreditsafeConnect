60473b5112c607cc0813c80e3c4b99988b033430
Daan Thelen <daan.thelen0@gmail.com>
2023-11-16 16:13:42 +0100

    Added StyleCop and applied all style suggestions.

--- CreditsafeConnect/HttpRequest.cs
+++ CreditsafeConnect/HttpRequest.cs
@@ -1,29 +1,36 @@
-﻿using System;
-using System.Net.Http;
-using System.Net.Http.Headers;
-using System.Text;
-using System.Threading.Tasks;
-using Newtonsoft.Json;
+﻿// <copyright file="HttpRequest.cs" company="Multitube Engineering B.V.">
+// Copyright (c) Multitube Engineering B.V. All rights reserved.
+// </copyright>
 
 namespace CreditsafeConnect
 {
+    using System;
+    using System.Net.Http;
+    using System.Net.Http.Headers;
+    using System.Text;
+    using System.Threading.Tasks;
+    using CreditsafeConnect.Models;
+    using Newtonsoft.Json;
+
     /// <summary>
     /// Class  <c>HttpRequest</c> handles all HTTP traffic and authentication between the caller and the Creditsafe API.
     /// </summary>
     public class HttpRequest
     {
-        private readonly HttpClient _client;
-        private readonly string _authEndpoint;
+        private readonly HttpClient client;
+        private readonly string authEndpoint;
 
         /// <summary>
-        /// Initializes a new instance of the <c>HttpRequest</c> class and authenticates to the Creditsafe API
+        /// Initializes a new instance of the <see cref="HttpRequest"/> class and authenticates to the Creditsafe API
         /// using the provided <paramref name="authEndpoint"/>, <paramref name="username"/>, and <paramref name="password"/>.
         /// </summary>
         /// <param name="baseAddress">Base address URI of the Creditsafe API.</param>
         /// <param name="authEndpoint">URI of the authentication endpoint of the Creditsafe API.</param>
         /// <param name="username">Username for authenticating to the Creditsafe API.</param>
         /// <param name="password">Password for authenticating to the Creditsafe API.</param>
-        /// <exception cref="ArgumentException">Thrown when one of the arguments provided is invalid.</exception>
+        /// <exception cref="ArgumentException">
+        /// Thrown when one of the arguments provided is invalid.
+        /// </exception>
         public HttpRequest(string baseAddress, string authEndpoint, string username, string password)
         {
             if (string.IsNullOrWhiteSpace(baseAddress)) throw new ArgumentException("BaseAddress value was invalid.");
@@ -33,27 +40,56 @@ namespace CreditsafeConnect
 
             baseAddress = baseAddress.Trim();
             authEndpoint = authEndpoint.Trim();
-            
+
             // HttpClient BaseAddress must always end with a '/'
-            if (!baseAddress.EndsWith("/"))
-            {
-                baseAddress += '/';
-            }
+            if (!baseAddress.EndsWith("/")) baseAddress += '/';
 
             // HttpClient requestUri should never start with a '/'
-            while (authEndpoint.StartsWith("/"))
-            {
-                authEndpoint = authEndpoint.Remove(0, 1);
-            }
+            while (authEndpoint.StartsWith("/")) authEndpoint = authEndpoint.Remove(0, 1);
 
-            _authEndpoint = authEndpoint;
+            this.authEndpoint = authEndpoint;
 
-            _client = new HttpClient
+            this.client = new HttpClient
             {
-                BaseAddress = new Uri(baseAddress)
+                BaseAddress = new Uri(baseAddress),
             };
 
-            Authenticate(username, password).GetAwaiter().GetResult();
+            this.client.DefaultRequestHeaders.Authorization = AuthenticationHeaderValue.Parse(this.Authenticate(username, password).GetAwaiter().GetResult());
+        }
+
+        /// <summary>
+        /// Requests a list of companies from the Creditsafe API based on the provided criteria.
+        /// </summary>
+        /// <param name="countries">Comma-separated list of two-letter countries codes (ISO 3166-1 alpha-2) for the countries to search in.</param>
+        /// <param name="name">Name of the company to be searched for.</param>
+        /// <param name="status">Status of the company to be searched for. The default value is <see cref="Status.Active"/>.</param>
+        /// <param name="pageSize">Number of companies to retrieve. Value should be between 1 and 100. The default value is 20.</param>
+        /// <returns>
+        /// List of companies as a string.
+        /// </returns>
+        /// <exception cref="ArgumentException">
+        /// Thrown when one of the arguments provided is invalid.
+        /// </exception>
+        /// <exception cref="HttpRequestException">
+        /// Thrown when the http response status code is not successful.
+        /// </exception>
+        public async Task<string> GetCompanies(
+            string countries,
+            string name,
+            string status = "Active",
+            int pageSize = 20)
+        {
+            if (string.IsNullOrWhiteSpace(countries)) throw new ArgumentException("Countries was invalid.");
+            if (string.IsNullOrWhiteSpace(name)) throw new ArgumentException("Name was invalid.");
+            if (!Enum.TryParse(status, out Status statusEnum)) throw new ArgumentException("Status was invalid.");
+            if (pageSize < 1 || pageSize > 100) throw new ArgumentException("Page size not allowed.");
+
+            HttpResponseMessage response = await this.client.GetAsync(
+                $"companies?countries={countries}&name={name}&status={statusEnum}&pageSize={pageSize}");
+
+            response.EnsureSuccessStatusCode();
+
+            return response.Content.ReadAsStringAsync().Result;
         }
 
         /// <summary>
@@ -62,69 +98,29 @@ namespace CreditsafeConnect
         /// </summary>
         /// <param name="username">Username for authenticating to the Creditsafe API.</param>
         /// <param name="password">Password for authenticating to the Creditsafe API.</param>
-        /// <returns></returns>
-        private async Task Authenticate(string username, string password)
+        /// <returns>JWT token as a string.</returns>
+        /// <exception cref="HttpRequestException">
+        /// Thrown when the http response status code is not successful.
+        /// </exception>
+        private async Task<string> Authenticate(string username, string password)
         {
             AuthRequest authRequest = new AuthRequest
             {
                 Username = username,
-                Password = password
+                Password = password,
             };
 
-            StringContent payload = new StringContent(JsonConvert.SerializeObject(authRequest), Encoding.UTF8, "application/json");
+            StringContent payload = new StringContent(
+                JsonConvert.SerializeObject(authRequest),
+                Encoding.UTF8,
+                "application/json");
 
-            HttpResponseMessage response = await _client.PostAsync(
-                this._authEndpoint, payload);
-
-            response.EnsureSuccessStatusCode();
-            
-            _client.DefaultRequestHeaders.Authorization = AuthenticationHeaderValue.Parse(response.Content.ReadAsStringAsync().Result);
-        }
-
-        /// <summary>
-        /// Requests a list of companies from the Creditsafe API based on the provided criteria.
-        /// </summary>
-        /// <param name="countries">Comma-separated list of two-letter countries codes (ISO 3166-1 alpha-2) for the countries to search in.</param>
-        /// <param name="name">Name of the company to be searched for.</param>
-        /// <param name="status">Status of the company to be searched for. The default value is <see cref="Status.Active"/></param>
-        /// <param name="pageSize">Number of companies to retrieve. Value should be between 1 and 100. The default value is 20.</param>
-        /// <returns>
-        /// List of companies as a string.
-        /// </returns>
-        /// <exception cref="ArgumentException"></exception>
-        public async Task<string> GetCompanies(string countries, string name, string status = "Active", int pageSize = 20)
-        {
-            if (string.IsNullOrWhiteSpace(countries)) throw new ArgumentException("Countries was invalid.");
-            if (string.IsNullOrWhiteSpace(name)) throw new ArgumentException("Name was invalid.");
-            if (!Enum.TryParse(status, out Status statusEnum)) throw new ArgumentException("Status was invalid.");
-            if (pageSize < 1 || pageSize > 100) throw new ArgumentException("Page size not allowed.");
-
-            HttpResponseMessage response = await _client.GetAsync(
-                $"companies?countries={countries}&name={name}&status={statusEnum}&pagesize={pageSize}");
+            HttpResponseMessage response = await this.client.PostAsync(
+                this.authEndpoint, payload);
 
             response.EnsureSuccessStatusCode();
-            
+
             return response.Content.ReadAsStringAsync().Result;
         }
-    }
-
-    /// <summary>
-    /// Represents a the payload to be sent in an authentication request to the Creditsafe authentication endpoint.
-    /// </summary>
-    public class AuthRequest
-    {
-        public string Username { get; set; }
-        public string Password { get; set; }
-    }
-
-    /// <summary>
-    /// Represents a company status.
-    /// </summary>
-    public enum Status
-    {
-        Active,
-        NonActive,
-        Pending,
-        Other
     }
 }
---
+++ CreditsafeConnect/Models/Status.cs
@@ -0,0 +1,17 @@
+﻿// <copyright file="Status.cs" company="Multitube Engineering B.V.">
+// Copyright (c) Multitube Engineering B.V. All rights reserved.
+// </copyright>
+
+namespace CreditsafeConnect.Models
+{
+    /// <summary>
+    /// Represents a company status.
+    /// </summary>
+    public enum Status
+    {
+        Active,
+        NonActive,
+        Pending,
+        Other,
+    }
+}
