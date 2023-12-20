76d323b06ce057062f0cf97b6f5a8ff156d591fe
Daan Thelen <daan.thelen0@gmail.com>
2023-12-20 10:16:29 +0100

    Moved company models to their own folder.

--- CreditsafeConnect/Models/CompanyModels/OfficeType.cs
+++ CreditsafeConnect/Models/CompanyModels/OfficeType.cs
@@ -2,19 +2,28 @@
 // Copyright (c) Multitube Engineering B.V. All rights reserved.
 // </copyright>
 
-namespace CreditsafeConnect.Models
+namespace CreditsafeConnect.Models.CompanyModels
 {
+    using System.ComponentModel.DataAnnotations;
+
     /// <summary>
     /// Represents a company office type.
     /// </summary>
     public enum OfficeType
     {
+        [Display(Name = "Nevenvestiging")]
         Branch,
+        [Display(Name = "Franchise")]
         Franchise,
+        [Display(Name = "Franchisor")]
         Franchisor,
+        [Display(Name = "Hoofdkantoor")]
         HeadOffice,
+        [Display(Name = "Overig")]
         Other,
+        [Display(Name = "Geregistreerd")]
         Registered,
+        [Display(Name = "Enkel kantoor")]
         SingleOffice,
     }
 }
