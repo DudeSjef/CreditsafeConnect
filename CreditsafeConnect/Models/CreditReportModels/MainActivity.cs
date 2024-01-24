4a6e5ce9da4ce86ef011a0a6e15866f007e1d8c2
Daan Thelen <daan.thelen0@gmail.com>
2024-01-24 13:35:47 +0100

    MainActivity code only accepts digits.

--- CreditsafeConnect/Models/CreditReportModels/MainActivity.cs
+++ CreditsafeConnect/Models/CreditReportModels/MainActivity.cs
@@ -4,15 +4,26 @@
 
 namespace CreditsafeConnect.Models.CreditReportModels
 {
+    using System.Linq;
+
     /// <summary>
     /// A class containing information about the company main activity.
     /// </summary>
     public class MainActivity
     {
+        private string code;
+
         /// <summary>
         /// Gets or sets the code for the company's main activity.
         /// </summary>
-        public string Code { get; set; }
+        public string Code
+        {
+            get => this.code;
+            set
+            {
+                this.code = new string(value.Where(char.IsDigit).ToArray());
+            }
+        }
 
         /// <summary>
         /// Gets or sets the description of the company's main activity.
